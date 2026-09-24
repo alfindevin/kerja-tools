132238c2912c903fa465be3123be09b203794496
alfindevin <alfinalfinalfin416@gmail.com>
2026-09-25 05:13:21 +0700

    feat: add components/SiteNav.tsx

---
+++ components/SiteNav.tsx
@@ -0,0 +1,15 @@
+import Link from 'next/link';
+
+export default function SiteNav() {
+  return (
+    <nav className="nav">
+      <div className="container nav-inner">
+        <Link className="brand" href="/">Kerja<span>Tools</span></Link>
+        <div className="nav-links">
+          <Link href="/#tools">Kalkulator</Link>
+          <Link href="/#tentang">Tentang</Link>
+        </div>
+      </div>
+    </nav>
+  );
+}
